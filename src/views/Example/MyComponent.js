e83e49330840e0e4f3da2bdb47f68bda9ce7f39f
khanhNguyen <nguyenngockhanhvthg@gmail.com>
2023-07-28 17:57:07 +0700

    video5

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,8 +2,35 @@ import React from "react";
 
 // để 1 class javascript hiểu rằng đâylà 1 react Component cần khai báo như  bên dưới => nó extends lại thức là kế thừa lại cái React.Component
 class MyComponent extends React.Component {
+  state = {
+    name: "Khanhne",
+    address: "CanTho",
+  };
+
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
   render() {
-    return <di>hello kanh ne he</di>;
+    // let name = "Khanh";
+    // //dùng {} để sử dụng javascritp trong JSX html
+
+    return (
+      <>
+        <div className="firstName">
+          {/* {/* {console.log("My name is: ", name)} */}
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          hello kanh ne he {this.state.name}
+        </div>
+        <div className="second">My address: {this.state.address}</div>
+      </>
+    );
   }
 }
 
