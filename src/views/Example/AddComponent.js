9b0217574730d54ab37c34895d94f719c4623dd4
khanhNguyen <nguyenngockhanhvthg@gmail.com>
2023-08-02 14:32:04 +0700

    video12

--- src/views/Example/AddComponent.js
+++ src/views/Example/AddComponent.js
@@ -1,3 +1,47 @@
 import React from "react";
-
-class AddComponent extends React.Component {}
+// nhơ qua MyComponent định nghĩa vì đó là nơi ta muốn làm con của nó
+class AddComponent extends React.Component {
+  state = {
+    titleJob: "",
+    salary: "",
+  };
+  handleChangTitleJob = (event) => {
+    this.setState({
+      titleJob: event.target.value,
+    });
+  };
+  handleChangSalary = (event) => {
+    this.setState({
+      salary: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(">> check data input: ", this.state);
+  };
+  render() {
+    return (
+      <form>
+        <label htmlFor="fname">titleJob:</label>
+        <br />
+        <input
+          type="text"
+          value={this.state.titleJob}
+          onChange={(event) => this.handleChangTitleJob(event)}
+        />
+        <br />
+        <label htmlFor="lname">salary:</label>
+        <br />
+        <input
+          type="text"
+          value={this.state.salary}
+          onChange={(event) => this.handleChangSalary(event)}
+        />
+        <br />
+        <br />
+        <input type="submit" onClick={(event) => this.handleSubmit(event)} />
+      </form>
+    );
+  }
+}
+export default AddComponent; // chỉ trả ra 1 thứ
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ChildComponents from "./ChildComponents";
+import AddComponent from "./AddComponent";
+
 class MyComponent extends React.Component {
   state = {
-    firstName: "",
-    lastName: "",
+    // firstName: "",
+    // lastName: "",
     arrJobs: [
       { id: "2000141", title: "Developers", salary: "100000" },
       { id: "2000142", title: "Testers", salary: "300000" },
@@ -11,52 +13,13 @@ class MyComponent extends React.Component {
     ],
   };
 
-  handleChangFirstName = (event) => {
-    this.setState({
-      firstName: event.target.value,
-    });
-  };
-  handleChangLastName = (event) => {
-    this.setState({
-      lastName: event.target.value,
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(">> check data input: ", this.state);
-  };
-
   render() {
     console.log("check call render: ", this.state);
     return (
       <>
-        <form action="/action_page.php">
-          <label htmlFor="fname">First name:</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.firstName}
-            onChange={(event) => this.handleChangFirstName(event)}
-          />
-          <br />
-          <label htmlFor="lname">Last name:</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.lastName}
-            onChange={(event) => this.handleChangLastName(event)}
-          />
-          <br />
-          <br />
-          <input type="submit" onClick={(event) => this.handleSubmit(event)} />
-        </form>{" "}
-        <ChildComponents
-          name={this.state.firstName}
-          age={"23"}
-          address={"CanTho"}
-          arrJobs={this.state.arrJobs}
-        />
+        <AddComponent />
+
+        <ChildComponents arrJobs={this.state.arrJobs} />
         {/* đây là 1 child của nesting */}
       </>
     );
