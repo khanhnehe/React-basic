2c700ef5cc74ad06cd84907bf694538753da3ff3
khanhNguyen <nguyenngockhanhvthg@gmail.com>
2023-08-15 15:54:16 +0700

    video18

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { withRouter } from "react-router";
 
 class Home extends React.Component {
+  //componentDiMount là 1 live cycle (vòng đời) của thằng react
+  //thằng home đầu tiên là nó chạy render ra trước rồi mới tới DiMount
+  componentDidMount() {
+    setTimeout(() => {
+      //setTimeout nhận 2 thâm số đầu tiên là function mà ta muốn thục thi,
+      //thứ 2 là cái time mà ta muốn nó thực thi
+
+      this.props.history.push("/todo"); // Home sẽ chuyển qua Todos trong 3s
+    }, 3000);
+  }
+
   render() {
+    console.log(">> check props: ", this.props);
+
     return (
       <>
         <div>Hello Home đây hãy coi ta như là 1 trang chủ</div>
@@ -10,4 +24,4 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+export default withRouter(Home);
