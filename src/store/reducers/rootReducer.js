e2e594cdeb721c61de8d4b25ebfc6caf092b37bb
khanhNguyen <nguyenngockhanhvthg@gmail.com>
2023-08-28 19:36:18 +0700

    video25

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -16,6 +16,7 @@ const initState = {
       name: "thêm cái thứ 3 check thử",
     },
   ],
+  post: [],
 };
 
 //rootReducers nhận 2 giá trị là state và action
@@ -24,8 +25,34 @@ const initState = {
 //action tức là cái react của we truyền action lên như thế nào
 // rootReducer đc coi như là công nhân => tức là khi we chạy vào trong cái reducer thì cuối cùng nó sẽ return 1 cái state
 const rootReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "DELETE_USER":
+      console.log(">> run into delete user: ", action);
+
+      let users = state.users; // như vậy đã lấy đc biến users của we ra
+      //gán lại users chính =
+      users = users.filter((item) => item.id !== action.payload.id);
+
+      // dùng break hay  return state đều đc ;
+      return { ...state, users };
+
+    case "CREATE_USER":
+      //dùng Math.floor(Math.random() *1000) để random id
+      let id = Math.floor(Math.random() * 1000);
+      //dung dấu `` để nói chuỗi
+      let user = { id: id, name: `random - ${id}` };
+
+      return {
+        // thăng users sẽ bằng đóng users hiện tại của we(vì vậy ta copy ...state lại)
+        // và cuối cùng là thằng user mới ta sẽ push nó vào
+        ...state,
+        users: [...state.users, user],
+      };
+    default:
+      return state;
+  }
+
   // gán state= inintState thì khi khởi động lên mặc định nó sẽ có giá trị là initState
-  return state;
 };
 
 export default rootReducer;
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -14,14 +14,39 @@ class Home extends React.Component {
     //   // this.props.history.push("/todo"); // Home sẽ chuyển qua Todos trong 3s
     // }, 3000);
   }
+  handleDeleteUser = (user) => {
+    console.log(">>> check delete user", user);
+    this.props.deleteUserRedux(user);
+  };
+
+  handleCreteUser = () => {
+    this.props.addUserRedux();
+  };
+
   render() {
     console.log(">> check props redux: ", this.props.dataRedux);
+
+    let litUsers = this.props.dataRedux;
 
     return (
       <>
         <div>Hello Home đây hãy coi ta như là 1 trang chủ</div>
         <div>
-          <img src={gojo} style={{ with: "200px", marginTop: "10px" }} />
+          <img src={gojo} style={{ width: "200px", marginTop: "10px" }} />
+        </div>
+        <div>
+          {/* litUsers && litUsers.length > 0 đk check xem litUsers có tòn tại ko */}
+          {litUsers &&
+            litUsers.length > 0 &&
+            litUsers.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {index + 1} - {item.name}
+                  <span onClick={() => this.handleDeleteUser(item)}> x</span>
+                </div>
+              );
+            })}
+          <button onClick={() => this.handleCreteUser()}>Add new</button>
         </div>
       </>
     );
@@ -34,8 +59,28 @@ const mapStateToProps = (state) => {
   return { dataRedux: state.users };
 };
 
+//lúc nào cx return là 1 Object nhé
+const mapDispatchToProps = (dispatch) => {
+  return {
+    //nhớ rằng action là 1 Object
+    deleteUserRedux: (userDelete) =>
+      dispatch({
+        type: "DELETE_USER",
+        payload: userDelete, //payload là hàm đàu vào của cái deleteUserRedux là userDelete
+      }),
+
+    //--add new user
+    addUserRedux: (userCreate) =>
+      dispatch({
+        type: "CREATE_USER",
+        payload: userCreate,
+      }),
+  };
+};
+
+//---
 // export default withRouter(Home);
 ///truyền mapStateToProps vào hàm connect, connect là sợi dây là liên kết
 ///giúp 2 ứng dụng react và redux kiểu như nó bắt tay vs nhau
 ///Nó chỉ bắt tay vs nhau trong component Home này thui s
-export default connect(mapStateToProps)(Color(Home));
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home));
